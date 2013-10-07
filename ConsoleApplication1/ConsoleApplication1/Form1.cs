f214210317b0d077bc900e317c9cb80c18098adb
AndreyMatselya <amatselya@gmail.com>
2013-10-07 22:56:33 +0300

    ыы

---
+++ ConsoleApplication1/ConsoleApplication1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			if (e.Url.ToString().IndexOf("af") != -1)
+			{
+				string accessToken = "";
+				int userId = 0;
+				Regex myReg = new Regex(@"(?<name>[\w\d\x5f]+)=(?<value>[^\x26\s]+)", RegexOptions.IgnoreCase | RegexOptions.Singleline);
+				foreach (Match m in myReg.Matches(e.Url.ToString()))
+				{
+					if (m.Groups["name"].Value == "af")
+					{
+						accessToken = m.Groups["value"].Value;
+					}
+					//else if (m.Groups["name"].Value == "user_id")
+					//{
+					//	userId = Convert.ToInt32(m.Groups["value"].Value);
+					//}
+					// еще можно запомнить срок жизни access_token - expires_in,
+					// если нужно
+				}
+				MessageBox.Show(String.Format("Ключ доступа: {0}\nUserID:", accessToken));
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			webBrowser1.Navigate(String.Format("http://google.com?af=144"));
+		}
+	}
+}
---
+++ WcfServiceLibrary1/ConsoleApplication1/Service1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+	// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+	public class Service1 : IService1
+	{
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite == null)
+			{
+				throw new ArgumentNullException("composite");
+			}
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+
+		public string GetData(double value)
+		{
+			return string.Format("You entered double: {0}", value);
+		}
+
+		public string GetDataWithService2(int value)
+		{
+			return string.Format("You entered double Iservice2: {0}", value);
+		}
+
+		public string SimpleProperty { get; set; }
+
+		public string GetSimpleProperty()
+		{
+			SimpleProperty = 34234.ToString(CultureInfo.InvariantCulture);
+			return SimpleProperty;
+		}
+	}
+}
