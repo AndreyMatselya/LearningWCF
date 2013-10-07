975453818fc87300f9b8df7b2cf1838e9a2ca105
AndreyMatselya <amatselya@gmail.com>
2013-10-08 00:01:44 +0300

    ыы

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace ConsoleApplication1
 			//var client = new Service1Client("BasicHttpBinding_IService1");
 
 
-			
+
 			EndpointAddress adress = new EndpointAddress("http://localhost:8733/trololo/blin");
 			var binding = new BasicHttpBinding();
 
@@ -34,19 +35,23 @@ namespace ConsoleApplication1
 			Console.WriteLine(client.GetDataWithService2(123123));
 			((ICommunicationObject)client).Close();*/
 
-			IService1 t = new Service1Client(binding, adress);
-			Console.WriteLine(t.GetDataWithService2(123123));
-			Console.WriteLine(t.GetData(000000000));
-			//Service1Client f = new Service2Client();
+			/*
+			Service2Client t = new Service1Client(binding, adress);
+			Console.WriteLine(t.GetDataWithService2(123123));*/
 
-			//var mexx = new MetadataExchangeClient(new EndpointAddress("http://katusha:8733/trololo/mex"));
-			//var metadata = mexx.GetMetadata();
-			//var importer = new WsdlImporter(metadata);
-			//var contracts = importer.ImportAllContracts();
-			//using (var xmlWriter = new XmlTextWriter("C:\\xcv.xml", Encoding.UTF8))
-			//{
-			//	metadata.WriteTo(xmlWriter);
-			//}
+
+
+			//Service1Client f = new Service2Client();
+			var httpBindingElement = new HttpTransportBindingElement();
+			httpBindingElement.MaxReceivedMessageSize *= 5;
+			var mexx = new MetadataExchangeClient(new CustomBinding(new BindingElement[] { new HttpTransportBindingElement() }));
+			var metadata = mexx.GetMetadata(new EndpointAddress("http://localhost:8733/trololo/mex"));
+			var importer = new WsdlImporter(metadata);
+			var contracts = importer.ImportAllContracts();
+			using (var xmlWriter = new XmlTextWriter("C:\\xcv.xml", Encoding.UTF8))
+			{
+				metadata.WriteTo(xmlWriter);
+			}
 
 		}
 	}
