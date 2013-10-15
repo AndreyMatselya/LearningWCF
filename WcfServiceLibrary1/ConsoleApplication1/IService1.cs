0062008535b90e5803a8eb7be98275540f516e6f
AndreyMatselya <amatselya@gmail.com>
2013-10-16 00:56:09 +0300

    ыы

--- WcfServiceLibrary1/ConsoleApplication1/IService1.cs
+++ WcfServiceLibrary1/ConsoleApplication1/IService1.cs
@@ -32,21 +32,21 @@ namespace ConsoleApplication1
 
 	// Use a data contract as illustrated in the sample below to add composite types to service operations.
 	// You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibrary1.ContractType".
-	//[Serializable]
-	[DataContract]
+	[Serializable]
+	//[DataContract]
 	public class CompositeType
 	{
 		bool boolValue = true;
 		string stringValue = "Hello ";
 
-		[DataMember]
+		//[DataMember]
 		public bool BoolValue
 		{
 			get { return boolValue; }
 			set { boolValue = value; }
 		}
-
-		[DataMember]
+		
+		//[DataMember]
 		public string StringValue
 		{
 			get { return stringValue; }
