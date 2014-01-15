9cf0ee5e950c3c0481764025f877f60045d10c46
������ <amatselya@gmail.com>
2014-01-15 08:21:23 +0300

    Merge branch 'master' of https://github.com/AndreyMatselya/LearningWCF
    Conflicts:
    	ConsoleApplication1/ConsoleApplication1/App.config
    	ConsoleApplication1/ConsoleApplication1/Program.cs
    	ConsoleApplication1/ConsoleApplication1/Service References/ServiceReference1/configuration.svcinfo
    	ConsoleApplication1/ConsoleApplication1/Service References/ServiceReference1/configuration91.svcinfo
    	ConsoleApplication1/ConsoleApplication1/Service References/ServiceReference1/service1.wsdl
    	WcfServiceLibrary1/ConsoleApplication1/App.config
    	WcfServiceLibrary1/ConsoleApplication1/IService1.cs
    	WcfServiceLibrary1/ConsoleApplication1/Service1.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 using System.ServiceModel;
+using System.Threading;
+using System.Threading.Tasks;
 using ConsoleApplication1.ServiceReference1;
 
 namespace ConsoleApplication1
@@ -13,22 +17,38 @@ namespace ConsoleApplication1
             //var service = new Service1Client("WSHttpBinding_IService1");
             //var service = new Service1Client("BasicHttpBinding_IService1");
             var service = new Service1Client("NetTcpBinding_IService1");
-            Console.WriteLine("InactivityTimeout: " + ((NetTcpBinding)service.Endpoint.Binding).ReliableSession.InactivityTimeout);
+            //Console.WriteLine("InactivityTimeout: " + ((NetTcpBinding)service.Endpoint.Binding).ReliableSession.InactivityTimeout);
             Console.WriteLine("OpenTimeout: " + service.Endpoint.Binding.OpenTimeout);
             Console.WriteLine("CloseTimeout: " + service.Endpoint.Binding.CloseTimeout);
             Console.WriteLine(default(DateTime));
 
-            
-		    for (var i = 0; i < 4; i++)
+		    var list = Create100Instanses().ToList();
+            foreach (var s in list)
 		    {
-                service.Init();
-                Console.WriteLine("Количество элементов: {0}",service.GetDict(i.ToString()).Count);
-                Console.WriteLine("SessionId: " + service.InnerChannel.SessionId);
+                //for (var i = 0; i < 20; i++)
+                //{
+                    s.Init();
+                    //Console.WriteLine("Количество элементов: {0}", service.GetDict(i.ToString()).Count);
+                    //Console.WriteLine("SessionId: " + service.InnerChannel.SessionId);
+                //}
+                Console.WriteLine(list.IndexOf(s));
 		    }
+
+
+
 			service.Close();
 			Console.ReadLine();
 		}
 
+        static IEnumerable<Service1Client> Create100Instanses()
+        {
+            var array = new List<Service1Client>();
+            for (var i = 0; i < 600; i++)
+            {
+                array.Add(new Service1Client("NetTcpBinding_IService1"));
+            }
+            return array;
+        }
 
 	}
     internal class Color
